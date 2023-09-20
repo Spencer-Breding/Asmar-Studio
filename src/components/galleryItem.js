caabe178fcaa011b8be11f6dd31bbb5c722d4aba
Spencer-Breding <55817041+Spencer-Breding@users.noreply.github.com>
2023-09-19 21:51:15 -0700

    Update galleryItem.js

--- src/components/galleryItem.js
+++ src/components/galleryItem.js
@@ -1,60 +1,24 @@
-"use client";
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
-import styles from '../styles/galleryItems.module.css';
+import styles from '../styles/galleryItems.module.css'
 
-export default function GalleryItem({ items, setCurrentIndex, priorityType, loadingType }) {
-    const [imageWrapperDimensions, setImageWrapperDimensions] = useState([]);
-
-    const adjustWrapperDimensions = (event, index) => {
-        const img = event.target;
-        if (img.complete) {
-            const imgWidth = img.naturalWidth;
-            const imgHeight = img.naturalHeight;
-
-            const aspectRatio = imgWidth / imgHeight;
-
-            const wrapper = img.parentNode;
-            const wrapperWidth = wrapper.clientWidth;
-            const wrapperHeight = wrapper.clientHeight;
-
-            let newWidth = wrapperHeight * aspectRatio;
-            let newHeight = wrapperHeight;
-
-            if (newWidth > wrapperWidth) {
-                newWidth = wrapperWidth;
-                newHeight = wrapperWidth / aspectRatio;
-            }
-
-            const newDimensions = [...imageWrapperDimensions];
-            newDimensions[index] = { width: newWidth, height: newHeight };
-            console.log('Setting new dimensions:', newDimensions);
-            setImageWrapperDimensions(newDimensions);
-        }
-    };
-
+export default function ImageGallery({ items, setCurrentIndex }) {
     return (
         <div className={styles.gallery}>
             {items.map((item, index) => (
                 <div key={index} className={styles.gallery_item}>
-                    <div
-                        className={styles.imageWrapper}
-                        style={{
-                            height: imageWrapperDimensions[index]?.height,
-                            width: imageWrapperDimensions[index]?.width
-                        }}
-                    >
+                    <div className={styles.image_wrapper}>
                         <Image
                             src={item.source}
                             alt={item.description}
-                            loading={loadingType}
-                            priority={priorityType}
+                            layout="responsive"  // Makes the image responsive
+                            width={1}  // The actual value doesn't matter when layout="responsive"
+                            height={1} // The actual value doesn't matter when layout="responsive"
+                            objectFit="contain"
+                            loading="lazy"
                             onClick={() => {
-                                setCurrentIndex(index);
-                                document.body.style.overflowY = 'hidden';
+                                setCurrentIndex(index)
+                                document.body.style.overflowY = 'hidden'
                             }}
-                            onLoad={(event) => adjustWrapperDimensions(event, index)}
-                            fill="true"
                         />
                     </div>
                     <p>{item.description}</p>
