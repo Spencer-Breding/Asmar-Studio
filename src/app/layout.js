8f2e7b6577e137fcde28ae6809c7ddc5ff28fbea
Spencer-Breding <Spencerbreding@gmail.com>
2023-10-11 10:01:27 -0700

    Accessibility update

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
                     name="description"
                     content="Design, Modeling, Sculpting, Prototypes, Metalwork, Woodwork, Fiberglass, Carpentry, Welding, Trade Shows, Set Building, Custom Furniture, Painting, Finishing, Project Management, Logistics, Resin Work"
                 />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, viewport-fit=cover"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover"/>
             </head>
             <body>
                 <Navbar />
