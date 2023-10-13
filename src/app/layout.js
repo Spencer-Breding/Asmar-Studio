33b3e774ee2ce0b3ede7a515c531315aa3285493
Spencer-Breding <Spencerbreding@gmail.com>
2023-10-13 11:40:20 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" className={goudy.className}>
             <head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
                 <meta
