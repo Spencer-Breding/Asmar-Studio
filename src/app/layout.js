8f7821f4d216be404562f631994dfd30099107a1
Spencer-Breding <Spencerbreding@gmail.com>
2023-10-21 08:02:58 -0700

    Added a csp

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import { Helmet } from 'react-helmet';
 import Navbar from '../components/navbar'
 import { Sorts_Mill_Goudy } from 'next/font/google'
 
@@ -12,6 +13,12 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" className={goudy.className}>
             <head>
+                <Helmet>
+                    <meta
+                        http-equiv="Content-Security-Policy"
+                        content="default-src 'self'; img-src 'self'; font-src 'self';"
+                    />
+                </Helmet>
                 <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
