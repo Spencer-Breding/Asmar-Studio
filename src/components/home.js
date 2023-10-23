f3299e7fe8bd6669d088b593efd714374bc76ec4
Spencer-Breding <Spencerbreding@gmail.com>
2023-10-22 18:46:03 -0700

    updating home component

--- src/components/home.js
+++ src/components/home.js
@@ -25,7 +25,6 @@ export default function Home() {
                 <source srcSet="./images/AsmarStudioLogo4k.webp" media="(min-width:240em)" />
                 <source srcSet="./images/AsmarStudioLogoLarge.webp" media="(min-width:160em)" />
                 <source srcSet="./images/AsmarStudioLogoMedium.webp" media="(min-width:124em)" />
-                <source srcSet="./images/AsmarStudioLogoSmall.webp" media="(min-width:80em)" />
                 <source srcSet="./images/AsmarStudioLogoMobile.webp" media="(min-width:52em)" />
                 <Image className={styles.logo} src="./images/AsmarStudioLogoMobileSmall.webp" alt="Asmar Studio Logo" priority={true} width={1} height={1} />
             </picture>
